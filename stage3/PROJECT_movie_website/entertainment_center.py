0c70337dbcd74dd917b2f150b7ebd0dc859bc9db
Carl Westman <carl.westman@kivra.com>
2017-03-20 17:39:14 +0100

    Added comments

--- stage3/PROJECT_movie_website/entertainment_center.py
+++ stage3/PROJECT_movie_website/entertainment_center.py
@@ -1,6 +1,12 @@
 import media
 import fresh_tomatoes
 
+""" 
+This file contains all data that the webpage presents.
+This file should be runned to dynamically render the 
+website from the data structure specified below and the 
+rendering functions specified in fresh_tomatoes.py
+""" 
 
 forest_gump = media.Movie(title="Forest Gump",
                           storyline="Forrest Gump is a simple man with a low I.Q. but good intentions. He is running through childhood with his best and only friend Jenny. His 'mama' teaches him the ways of life and leaves him to choose his destiny. Forrest joins the army for service in Vietnam, finding new friends called Dan and Bubba, he wins medals, creates a famous shrimp fishing fleet, inspires people to jog, starts a ping-pong craze, creates the smiley, writes bumper stickers and songs, donates to people and meets the president several times. However, this is all irrelevant to Forrest who can only think of his childhood sweetheart Jenny Curran, who has messed up her life. Although in the end all he wants to prove is that anyone can love anyone.",
