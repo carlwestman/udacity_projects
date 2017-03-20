aea98d09596ff023e54e35ef4375300ddac4025d
Carl Westman <carl.westman@kivra.com>
2017-03-20 18:00:07 +0100

    Added docstrings to classes

--- stage3/PROJECT_movie_website/media.py
+++ stage3/PROJECT_movie_website/media.py
@@ -2,6 +2,23 @@ import webbrowser
 
 
 class media():
+    """ 
+        Defines media class.
+        Media class is parent class of Movie and TvShow class and can be extended with 
+        other media classes when need there is a need.
+        
+        Class attribute:
+            The media class contains 7 attibutes and should be initiated will all 7.
+            title (str) -> the title of the TvShow or Movie.
+            storyline (str) -> the storyline of the TvShow or Movie
+            trailer_youtube_url (str) -> URL to youtube trailer
+            actors (list of strings) -> List of Actors that are in TvShow or Movie
+            imdb_rating (float) -> IMDB rating from IMDB.com
+            genre (List of strings) -> List of genres that apply for TvShow or Movie
+            poster_image_url (str) -> URL to a poster img
+            
+                
+    """
     def __init__(self, title, storyline, trailer_youtube_url, actors, imdb_rating, genre, poster_image_url):
         self.title = title
         self.storyline = storyline
@@ -13,7 +30,28 @@ class media():
 
 
 class Movie(media):
-    """Extension of MotionPicture Class, contains data about Movies."""
+    """Extension of media Class, contains data about Movies.
+    
+        Movies is a child class to Media.
+    
+        Class attribute:
+            The Movie class contains 10 attibutes and should be initiated with 9 below. 
+            The last attribute is set automatically 
+            title (str) -> the title of the Movie.
+            storyline (str) -> the storyline of the Movie
+            trailer_youtube_url (str) -> URL to youtube trailer
+            actors (list of strings) -> List of Actors that are in Movie
+            imdb_rating (float) -> IMDB rating from IMDB.com
+            genre (List of strings) -> List of genres that apply for Movie
+            poster_image_url (str) -> URL to a poster img
+            year (int) -> Year the movie was released
+            director (str) -> Director name
+            
+            Automatically set attributes:
+            type (str) -> 'Movie', all movies are of type Movie, this is set to be able
+            to distiguise between different media types in UI
+    
+    """
     def __init__(self, title, trailer_youtube_url, storyline,
                  poster_image_url, actors, imdb_rating, genre, year, director):
 
@@ -40,7 +78,30 @@ class Movie(media):
 
 
 class TvShow(media):
-    """Extension of MotionPicture Class, contains data about TV Show."""
+    """Extension of media Class, contains data about TvShow.
+    
+        TvShow is a child class to Media.
+    
+        Class attribute:
+            The Movie class contains 11 attibutes where one is set automatically and one is optional.
+            title (str) -> the title of the TvShow.
+            storyline (str) -> the storyline of the TvShow.
+            trailer_youtube_url (str) -> URL to youtube trailer
+            actors (list of strings) -> List of Actors that are in TvShow
+            imdb_rating (float) -> IMDB rating from IMDB.com
+            genre (List of strings) -> List of genres that apply for TvShow
+            poster_image_url (str) -> URL to a poster img
+            start_year (int) -> Year the of the first season
+            seasons (int) -> Number of seasons
+
+            Optional attributes
+            end_year (int) -> Year of the last season, if not set (ie, TvShow still on air) end_year = None
+            
+            Automatically set attributes:
+            type (str) -> 'Movie', all movies are of type Movie, this is set to be able
+            to distiguise between different media types in UI
+    
+    """
     def __init__(self, title, storyline, trailer_youtube_url, poster_image_url, actors, imdb_rating, genre, start_year, seasons, end_year=None):
 
         media.__init__(self,
